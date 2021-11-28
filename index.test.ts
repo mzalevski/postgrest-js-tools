dba88f7a872ddeb829212ccbe5615008476c7eb2
mzalevski <kontakt@mzalevski.pl>
2021-11-28 11:11:30 +0100

    init

---
+++ index.test.ts
@@ -0,0 +1,70 @@
+import { getShape, getFields } from ".";
+
+type CheckBase = { id: string; place_id: string; user_id: string };
+type PlaceBase = { id: string; title: string };
+type UserBase = { id: string; name: string };
+
+type Check = CheckBase & {
+  /** user relation */
+  user: UserBase;
+  /** place relation */
+  place: PlaceBase;
+};
+
+test("some fields, no relations & no omissions", () => {
+  const shape = getShape<Check>()({
+    id: true,
+  });
+
+  const fields = getFields(shape);
+
+  expect(fields).toBe("id");
+});
+
+test("some fields, relations & no omissions", () => {
+  const shape = getShape<Check>()({
+    id: true,
+    place: { _: "place_id", id: true },
+    user: { _: "user_id", id: true },
+  });
+
+  const fields = getFields(shape);
+
+  expect(fields).toBe("id,place:place_id(id),user:user_id(id)");
+});
+
+test("many fields, relations & no omissions", () => {
+  const shape = getShape<Check>()({
+    id: true,
+    place: { _: "place_id", id: true, title: true },
+    user: { _: "user_id", id: true, name: true },
+  });
+
+  const fields = getFields(shape);
+
+  expect(fields).toBe("id,place:place_id(id,title),user:user_id(id,name)");
+});
+
+test("many fields, relations & some omissions", () => {
+  const shape = getShape<Check>()({
+    id: true,
+    place: { _: "place_id", id: true, title: true },
+    user: { _: "user_id", id: true, name: false },
+  });
+
+  const fields = getFields(shape);
+
+  expect(fields).toBe("id,place:place_id(id,title),user:user_id(id)");
+});
+
+test("many fields, relations & many omissions", () => {
+  const shape = getShape<Check>()({
+    id: false,
+    place: { _: "place_id", id: false, title: true },
+    user: { _: "user_id", id: true, name: false },
+  });
+
+  const fields = getFields(shape);
+
+  expect(fields).toBe("place:place_id(title),user:user_id(id)");
+});
---
+++ index.ts
@@ -0,0 +1,53 @@
+export const getFields = (shape: Record<string, any>) => {
+  const joins: Record<string, string> = {};
+
+  let lastKey = "";
+
+  const fields = `${JSON.stringify(shape, (k, v) => {
+    if (typeof v === "object") {
+      lastKey = k;
+      return v;
+    }
+
+    if (v === true) {
+      return "";
+    }
+
+    if (k === "_") {
+      joins[lastKey] = v;
+    }
+
+    return undefined;
+  })
+    .replace(/[": \n]/g, "")
+    .replace(/\{/g, "(")
+    .replace(/\}/g, ")")
+    .slice(1, -1)}`;
+
+  if (Object.keys(joins).length > 0)
+    return fields.replace(
+      new RegExp(Object.keys(joins).join("|"), "g"),
+      (m) => `${m}:${joins[m]}`
+    );
+
+  return fields;
+};
+
+type MakePropTypesBoolean<T, A = T> = T extends object
+  ? { [K in keyof T]: MakePropTypesBoolean<Partial<T[K]>, T> } & {
+      /** which field to join by */
+      _: keyof A;
+    }
+  : boolean;
+
+type ParseReturnType<T, U> = U extends object
+  ? Omit<{ [K in keyof U]: ParseReturnType<T[Extract<K, keyof T>], U[K]> }, "_">
+  : T;
+
+export const getShape =
+  <T>() =>
+  <U extends Partial<MakePropTypesBoolean<T>>>(
+    fields: U
+  ): ParseReturnType<T, U> => {
+    return fields as any;
+  };
